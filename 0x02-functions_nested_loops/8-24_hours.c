02706139f91123451108449fd0cc123112a5cbd7
Ibematthew1 <Ibematthew1@gmail.com>
2023-02-21 03:53:12 -0800

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * jack_bauer - print out every minutes
+ *
+ * Description: print out the required results
+ *
+ * Return: return the integer value 0
+ *
+ */
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
