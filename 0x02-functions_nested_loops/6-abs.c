eeb3ce495ed676a2183cba3fc5d2e1deb4f3503a
Ibematthew1 <Ibematthew1@gmail.com>
2023-02-21 03:42:25 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - get the aboslute number of an integer
+ * @x: the value that function return the absolute number
+ *
+ * Description: return the required results
+ *
+ * Return: return integer value 0
+ *
+ */
+int _abs(int x)
+{
+	if (x >= 0)
+		return (x);
+	else
+		return (x * -1);
+	return (0);
+}
