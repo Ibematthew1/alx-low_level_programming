ce1e1d8b1405b7177f4c61820b08acf39e08fdc4
Ibematthew1 <Ibematthew1@gmail.com>
2023-02-23 07:06:39 -0800

    Hello, Trying to fix this

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,36 +1,24 @@
 #include "main.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-
-int _putchar(char c);
-
 /**
- * print_number - prints numbers
- * @n: number to be printed
- * Return:void
+ * print_number - prints an integer
+ * @n: integer to be printed
  */
-
 void print_number(int n)
 {
+	unsigned int n1;
 
-	unsigned int n1 = 0;
-
-	if  (n < 0)
+	if (n < 0)
 	{
 		n1 = -n;
 		_putchar('-');
-	}
-
-	else
+	} else
 	{
 		n1 = n;
 	}
-
 	if (n1 / 10)
 	{
 		print_number(n1 / 10);
 	}
-
 	_putchar((n1 % 10) + '0');
 }
