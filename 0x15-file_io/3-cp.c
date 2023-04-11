ee0b2b6b5375cde0684f632cebbfdc512968064b
Ibematthew1 <Ibematthew1@gmail.com>
2023-04-11 04:21:45 -0700

    Updated

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,7 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+/**
+ * check97 - checks for the correct number of arguments
+ * @argc: number of arguments
+ *
+ * Return: void
+ */
 void check97(int argc)
 {
 	if (argc != 3)
@@ -12,6 +17,15 @@ void check97(int argc)
 	}
 }
 
+/**
+ * check98 - checks that file_from exists and can be read
+ * @check: checks if true of false
+ * @file: file_from name
+ * @fd_from: file descriptor of file_from, or -1
+ * @fd_to: file descriptor of file_to, or -1
+ *
+ * Return: void
+ */
 void check98(ssize_t check, char *file, int fd_from, int fd_to)
 {
 	if (check == -1)
@@ -25,6 +39,15 @@ void check98(ssize_t check, char *file, int fd_from, int fd_to)
 	}
 }
 
+/**
+ * check99 - checks that file_to was created and/or can be written to
+ * @check: checks if true of false
+ * @file: file_to name
+ * @fd_from: file descriptor of file_from, or -1
+ * @fd_to: file descriptor of file_to, or -1
+ *
+ * Return: void
+ */
 void check99(ssize_t check, char *file, int fd_from, int fd_to)
 {
 	if (check == -1)
@@ -38,6 +61,13 @@ void check99(ssize_t check, char *file, int fd_from, int fd_to)
 	}
 }
 
+/**
+ * check100 - checks that file descriptors were closed properly
+ * @check: checks if true or false
+ * @fd: file descriptor
+ *
+ * Return: void
+ */
 void check100(int check, int fd)
 {
 	if (check == -1)
@@ -46,7 +76,13 @@ void check100(int check, int fd)
 		exit(100);
 	}
 }
-
+/**
+ * main - opies the content of a file to another file.
+ * @argc: number of arguments passed
+ * @argv: array of pointers to the arguments
+ *
+ * Return: 0 on success
+ */
 int main(int argc, char *argv[])
 {
 	int fd_from, fd_to, close_to, close_from;
